8ac8e0745e859607a289d29aa3627db4a3a71e91
Shakti Prakash Das <Dasshakti659@gmail.com>
2026-04-25 23:47:18 +0530

    feat: add user signup with password hashing  (BCrygt) and security config

--- src/main/java/com/taskmanager/app/service/UserService.java
+++ src/main/java/com/taskmanager/app/service/UserService.java
@@ -5,6 +5,7 @@ import com.taskmanager.app.dto.UserRequest;
 import com.taskmanager.app.model.User;
 import com.taskmanager.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,15 +15,21 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
     public User register(UserRequest request){
 
         //check if user already exist
         if(userRepository.findByUsername(request.getUsername()).isPresent()){
             throw new RuntimeException("Username already exists");
         }
+
         User user=new User();
         user.setUsername(request.getUsername());
-        user.setPassword(request.getPassword());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         return userRepository.save(user);
     }
